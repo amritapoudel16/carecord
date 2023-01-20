5ccadebd811aff474d21e18d07f8e0f896a3a915
amritapoudel16 <amritapoudel16@gmail.com>
2023-01-20 21:42:25 +0545

    last commit

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import Test from "./test";
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-    {/* <Test /> */}
-  </BrowserRouter>
+	<React.StrictMode>
+		<BrowserRouter>
+			<App />
+			{/* <Test /> */}
+		</BrowserRouter>
+	</React.StrictMode>
 );
